0a82d32bb3c24273bc85767887f9a16626388ac8
Jorge Pascual Fuentecilla <jorgepasf@gmail.com>
2025-01-13 16:14:55 +0100

    routas creadas

---
+++ client/src/routes/index.jsx
@@ -0,0 +1,23 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+const Router = () => {
+    return (
+         <BrowserRouter>
+            <Routes>
+                <Route path= "/" element = {<Home />} />               
+                <Route path= "/login" element = {<Login />} />
+                <Route path= "/registro" element = {<Register />} />
+                <Route path= "/catalogo" element = {<Catalogo />} />
+                <Route path= "/hoja-de-oferta" element = {<OfferPage />} />
+                <Route path= "/subasta" element = {<Subasta />} />
+                <Route path= "/contacto" element = {<Contact />} />
+            </Routes>
+         </BrowserRouter>
+    
+         
+    );
+
+
+
+
+};
