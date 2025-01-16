7c316ae69edc7c170cac036bdbd4386b0db49ba7
gestefania2 <gestefania2@gmail.com>
2025-01-16 17:14:50 +0100

    he añadido fuentes y he configurado el navbar para que sea responsive y he incluido logo bidly

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -4,22 +4,23 @@ import LoginModal from '../modals/LoginModal';
 import './Navbar.css';
 
 const Navbar = () => {
-    const navigate = useNavigate(); 
+    const navigate = useNavigate();
     const [searchQuery, setSearchQuery] = useState('');
     const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
     const [isLoginOpen, setIsLoginOpen] = useState(false);
 
     const toggleMenu = () => {
-        setIsMobileMenuOpen(false);
+        setIsMobileMenuOpen(!isMobileMenuOpen);
     };
 
     const handleLoginClick = (e) => {
         e.preventDefault(); // Previene la navegación a /login
         setIsLoginOpen(true);
     };
+
     const handleRegisterClick = (e) => {
         e.preventDefault(); // Previene la navegación a /register
-        navigate ('/registro');
+        navigate('/registro');
     };
 
 
@@ -31,14 +32,20 @@ const Navbar = () => {
                     <div className="navbar-content">
                         {/* Logo */}
                         <div className="navbar-logo">
-                            <a href="/">BIDLY</a>
+                            <Link to="/" >
+                                <img
+                                    className='logo'
+                                    src="/logo_bidly.png"
+                                    alt="bidly"
+                                />
+                            </Link>
                         </div>
 
                         {/* Main Navigation */}
                         <div className="navbar-links">
-                            <Link to="/acerca-de" className="menu-item" onClick={toggleMenu}>ACERCA DE</Link>
-                            <Link to="/catalogo" className="menu-item" onClick={toggleMenu}>CATÁLOGO</Link>
-                            <Link to="/contacto" className="menu-item" onClick={toggleMenu}>CONTACTO</Link>
+                            <Link to="/acerca-de" className="menu-item">ACERCA DE</Link>
+                            <Link to="/catalogo" className="menu-item">CATÁLOGO</Link>
+                            <Link to="/contacto" className="menu-item">CONTACTO</Link>
                         </div>
 
                         {/* Right side - Search and Auth */}
@@ -47,19 +54,37 @@ const Navbar = () => {
                             <div className="search-container">
                                 <input
                                     type="text"
-                                    placeholder="Search by keyword"
+                                    placeholder="Buscar artículo..."
                                     value={searchQuery}
                                     onChange={(e) => setSearchQuery(e.target.value)}
                                 />
                                 <button className="search-button">
-                                    <span>&#128269;</span>
+                                    <svg
+                                        fill="none"
+                                        viewBox="0 0 20 20"
+                                        width="20"
+                                        height="20"
+                                    >
+                                        <path
+                                            d="M11.856 11.856l4.3 4.3"
+                                            stroke="#222"
+                                            strokeLinecap="round"
+                                            strokeLinejoin="round"
+                                        />
+                                        <path
+                                            d="M12.693 8.155a4.538 4.538 0 11-9.077 0 4.538 4.538 0 019.077 0z"
+                                            stroke="#222"
+                                            strokeLinecap="round"
+                                            strokeLinejoin="round"
+                                        />
+                                    </svg>
                                 </button>
                             </div>
 
                             {/* Auth Links */}
                             <div className="auth-links">
                                 <a href="/login" onClick={handleLoginClick}>Iniciar sesión</a>
-                                <a href="/registro"onClick={handleRegisterClick}>Regístrate</a>
+                                <a href="/registro" onClick={handleRegisterClick}>Regístrate</a>
                             </div>
                         </div>
 
@@ -75,16 +100,21 @@ const Navbar = () => {
                     </div>
                 </div>
 
+
                 {/* Mobile Menu */}
                 <div className={`mobile-menu ${isMobileMenuOpen ? 'active' : ''}`}>
                     <Link to="/acerca-de" className="menu-item" onClick={toggleMenu}>ACERCA DE</Link>
                     <Link to="/catalogo" className="menu-item" onClick={toggleMenu}>CATÁLOGO</Link>
                     <Link to="/contacto" className="menu-item" onClick={toggleMenu}>CONTACTO</Link>
+                    <div className="auth-mobile-links">
+                        <a href="/login" className="menu-item-login" onClick={handleLoginClick}>Iniciar sesión</a>
+                        <a href="/registro" className="menu-item-register" onClick={handleRegisterClick}>Regístrate</a>
+                    </div>
                 </div>
             </nav>
 
             {/* Modal de Login */}
-            <LoginModal 
+            <LoginModal
                 isOpen={isLoginOpen}
                 onClose={() => setIsLoginOpen(false)}
             />
