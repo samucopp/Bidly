42a276584f89d17beeda8cfbc033fda759f04c9a
samucop95 <samucop@icloud.com>
2025-01-17 17:53:43 +0100

    nuevos middlewares para comprobar la autoria de las notificaciones

--- server/src/helpers/middleware.js
+++ server/src/helpers/middleware.js
@@ -1,6 +1,7 @@
 import jwt from "jsonwebtoken";
 import Auction from "../models/auctionModel.js";
 import Bid from "../models/bidModel.js";
+import Notification from "../models/notificationModel.js";
 
 function checkAuthorization(req, res, next) {
     try {
@@ -69,8 +70,45 @@ function checkBidOwnership(req, res, next) {
     }
 }
 
+async function checkNotificationOwnership(req, res, next) {
+    try {
+        const { notificationId } = req.params;
+        const userId = req.user.id;
+        console.log(userId, notificationId);
+        const notification = await Notification.findById(notificationId);
+        if (!notification) {
+            return res.status(404).json({ message: "Notificación no encontrada." });
+        }
+
+        if (notification.userId.toString() !== userId.toString()) {
+            return res.status(403).json({ message: "No tienes permiso para realizar esta acción." });
+        }
+
+        next();
+    } catch (error) {
+        return res.status(500).json({ message: "Error al verificar la notificación", error: error.message });
+    }
+}
+
+async function checkUserNotificationsOwnership(req, res, next) {
+    try {
+        const { userId } = req.params;
+        const authenticatedUserId = req.user.id;
+
+        if (userId !== authenticatedUserId) {
+            return res.status(403).json({ message: "No tienes permiso para acceder a las notificaciones de otro usuario." });
+        }
+
+        next();
+    } catch (error) {
+        return res.status(500).json({ message: "Error al verificar la propiedad de las notificaciones", error: error.message });
+    }
+}
+
 export {
     checkAuthorization,
     checkAuctionOwnership,
-    checkBidOwnership
+    checkBidOwnership,
+    checkNotificationOwnership,
+    checkUserNotificationsOwnership
 };
