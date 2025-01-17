e110dd656e0a96ac774551cb70b40cf3d4a7e8ef
samucop95 <samucop@icloud.com>
2025-01-17 17:55:29 +0100

    nueva ruta creada, para eliminar todas las notificaciones. middlewares integrados en las rutas de notificacaciones.

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -1,15 +1,9 @@
 import User from "../models/userModel.js";
-import { hashPassword, verifyPassword } from "../config/bcrypt.js";
+import jwt from "jsonwebtoken";
 
 async function register(req, res) {
     try {
         const { name, email, password, passwordRepeat, address } = req.body;
-        const hash = await hashPassword(password);
-        if (!hash) {
-            return res.status(400).json({
-                message: "Error al hashear la contraseña",
-            });
-        }
         if (password !== passwordRepeat) {
             return res.status(400).json({
                 message: "Las contraseñas no coinciden",
@@ -24,7 +18,7 @@ async function register(req, res) {
         const user = await User.create({
             name,
             email,
-            password: hash,
+            password,
             address,
         });
         return res.status(201).json(user);
@@ -50,13 +44,14 @@ async function login(req, res) {
                 message: "El correo no esta registrado",
             });
         }
-        const isMatch = await verifyPassword(password, user.password);
+        const isMatch = await user.comparePassword(password);
         if (!isMatch) {
             return res.status(400).json({
                 message: "La contraseña es incorrecta",
             });
         }
-        return res.status(200).json(user);
+        const token = jwt.sign({ id: user._id, email: user.email }, process.env.JWT_SECRET, { expiresIn: "1d" });
+        return res.status(200).json({message: "Login exitoso", token});
     } catch (error) {
         console.error(error);
         return res.status(500).json({
