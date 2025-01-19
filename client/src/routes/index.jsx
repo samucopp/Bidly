036d47f22e7c49cfc3fcfef04bf3cb184c7e7ea2
JorgePascualFuentecilla <jorgepasf@gmail.com>
2025-01-19 21:33:09 +0100

    Subasta ya recibe datos

--- client/src/routes/index.jsx
+++ client/src/routes/index.jsx
@@ -26,7 +26,7 @@ const router = createBrowserRouter([
         element: <AboutUs />,
       },
       {
-        path: "auction",
+        path: "auction/:auctionId",
         element: <Subasta />
       },
       {
