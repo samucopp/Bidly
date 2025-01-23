72c8ee02e650a113dacd93ef61d5dd048dbe46cd
samucopp <samucop@icloud.com>
2025-01-23 19:51:03 +0100

    agregada funcionalidad para añadir subastas a tus favoritos. implementado boton toggle corazon. modificaciones de los componentes necesarios y creacion de la hoja de css para las tarjetas del catalogo.

--- client/src/components/auction/AuctionList.jsx
+++ client/src/components/auction/AuctionList.jsx
@@ -1,17 +1,22 @@
 import React from "react";
-import Tarjeta from "../Tarjeta";
-import "./AuctionList.css";  
+import Tarjeta from "../auction-card/Tarjeta";
+import "./AuctionList.css";
 
-const AuctionList = ({auctions}) => {   
-    
+const AuctionList = ({ auctions }) => {
+
 
     return (
-        
-            <div className="tarjetas-container">  
-                {auctions.map((auction) => (
-                    <Tarjeta key={auction._id} datosPuja={auction} />
-                ))}
-            </div>
+
+        <div className="tarjetas-container">
+            {auctions.map((auction) => (
+                <Tarjeta
+                    key={auction._id}
+                    datosPuja={auction}
+                    favoriteIcon="/fav-icons/heart-full.png"
+                    notFavoriteIcon="/fav-icons/heart-empty.png"
+                />
+            ))}
+        </div>
     );
 };
 
